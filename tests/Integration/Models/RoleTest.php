410e25fd2a8bcb3d058b101b49dcdd5d3971b05b
curder <q.curder@gmail.com>
2022-01-22 15:20:17 +0800

    Basic Models Test

---
+++ tests/Integration/Models/RoleTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Integration\Models;
+
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
+
+class RoleTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function it_has_roles_table(): void
+    {
+        $this->assertTrue(Schema::hasTable('roles'));
+    }
+
+    /** @test */
+    public function it_has_some_columns(): void
+    {
+        $columns = [
+            'id',
+            'name',
+            'display_name',
+            'description',
+            'created_at',
+            'updated_at',
+        ];
+
+        $this->assertSame(Schema::getColumnListing('roles'), $columns);
+        $this->assertTrue(Schema::hasColumns('roles', $columns));
+    }
+}
---
+++ tests/Integration/Models/RoleUserTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Integration\Models;
+
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
+
+class RoleUserTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function it_has_role_user_table(): void
+    {
+        $this->assertTrue(Schema::hasTable('role_user'));
+    }
+
+    /** @test */
+    public function it_has_some_columns(): void
+    {
+        $columns = [
+            'user_id',
+            'role_id',
+            'description',
+            'created_at',
+            'updated_at',
+        ];
+
+        $this->assertSame(Schema::getColumnListing('role_user'), $columns);
+        $this->assertTrue(Schema::hasColumns('role_user', $columns));
+    }
+}
